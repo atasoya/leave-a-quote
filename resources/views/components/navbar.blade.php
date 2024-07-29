aa452622fcb8e0390bcc63a5c6024ede967ad01b
atasoya <ataatasoy2013@gmail.com>
2024-07-30 00:40:24 +0300

    add hot to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -19,7 +19,7 @@
           tabindex="0"
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
           <li class="text-black"><a href="/popular">Popular</a></li>
-          <li class="text-black"><a>Hot</a></li>
+          <li class="text-black"><a href="/hot-qoutes">Hot</a></li>
         </ul>
       </div>
       <a class="btn btn-ghost text-xl" href="/">Leave a Quote</a>
@@ -27,7 +27,7 @@
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
         <li><a href="/popular">Popular</a></li>
-        <li><a>Hot</a></li>
+        <li><a href="/hot-qoutes">Hot</a></li>
       </ul>
     </div>
     <div class="navbar-end">
